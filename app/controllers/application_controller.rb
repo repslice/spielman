e2ade40c52f997b1befab28ea07e4ccc373feb9f
Repslice <repslice@gmail.com>
2012-12-11 23:33:45 -0500

    Initial configuration is done

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,4 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+  config.filter_parameters += [:password, :password_confirmation]
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spielman::Application.config.secret_token = '3bfb6654b80a32155d9cf01d1fff6f83993686a9534c1601d7600ab2ff914c9fc232709b5f295ea840a3dcb72f839ff621a5294bd30b75d826dd4caa38f5dfe5'
